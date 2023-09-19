a149f0f5f775da68f1aa4460fdc19ed3659e1be3
Nikolay <cmirnov_3@mail.ru>
2023-09-19 16:36:53 +0300

    задача 58

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 58: Задайте две матрицы. 
+// Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matrix1 = {
+            { 2, 4 },
+            { 3, 2 }
+        };
+
+        int[,] matrix2 = {
+            { 3, 4 },
+            { 3, 3 }
+        };
+
+        int[,] result = MultiplyMatrices(matrix1, matrix2);
+
+        Console.WriteLine("Результирующая матрица:");
+
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                Console.Write(result[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
+    {
+        int rows1 = matrix1.GetLength(0);
+        int cols1 = matrix1.GetLength(1);
+        int cols2 = matrix2.GetLength(1);
+
+        int[,] result = new int[rows1, cols2];
+
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < cols2; j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < cols1; k++)
+                {
+                    sum += matrix1[i, k] * matrix2[k, j];
+                }
+                result[i, j] = sum;
+            }
+        }
+
+        return result;
+    }
+}
